b0a493cb5cf24eb7df8658e11312f42434f77db8
Richouf95 <wourichouf95@gmail.com>
2023-09-08 14:42:50 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - alphABET
+ * Return: 0
+*/
+
+int main(void)
+{
+	char l;
+	char u;
+
+	l = 'a';
+	u = 'A';
+	while(l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	while(u <= 'Z')
+	{
+		putchar(u);
+		u++;
+	}
+	putchar('\n');
+	return (0);
+}
