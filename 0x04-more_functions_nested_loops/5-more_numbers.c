aa9f104f8b4725e04cd4940726ffbbb189e50a40
Richouf95 <wourichouf95@gmail.com>
2023-09-14 16:09:09 -0700

    Print 10 time numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - from 0 to 14 10 times
+ *
+ * Return: 0
+*/
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+}
