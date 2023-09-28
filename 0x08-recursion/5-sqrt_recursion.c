2ba2b7c0f1b96208add70ea48c8926af4aa21572
Richouf95 <wourichouf95@gmail.com>
2023-09-28 07:38:29 -0700

    Natural square

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,5 +8,25 @@
 
 int _sqrt_recursion(int n)
 {
-	
+	if (n < 0)
+		return (-1);
+	return (current_recursion(n, 0));
+}
+
+/**
+ * current_recursion - where find natural square
+ * @n: input
+ * @x: input
+ * Return: result
+*/
+
+int current_recursion(int n, int x)
+{
+	if (x * x > n)
+		return (-1);
+
+	if (x * x == n)
+		return (x);
+
+	return (current_recursion(n, x + 1));
 }
