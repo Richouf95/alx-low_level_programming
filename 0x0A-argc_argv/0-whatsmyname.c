a635128a0be4709d9b141730c6b2a876d871219f
Richouf95 <wourichouf95@gmail.com>
2023-10-02 13:32:21 -0700

    Fix Prinf progral name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print prog name
+ * @argc: number of arg
+ * @argv: list of arg
+ * Return: 0
+*/
 
 int main(int argc, char **argv)
 {
-	printf("./%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
