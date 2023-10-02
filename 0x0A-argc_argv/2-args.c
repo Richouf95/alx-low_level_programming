4c70117b2bc5c8c0c2c82b0cab8c0381517df5a7
Richouf95 <wourichouf95@gmail.com>
2023-10-02 15:39:26 -0700

    Listing args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print all args
+ * @argc: number of arg
+ * @argv: list of arg
+ * Return: 0;
+*/
+
+int main(int argc, int *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
