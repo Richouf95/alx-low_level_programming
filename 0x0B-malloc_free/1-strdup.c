fd785bb43dd73faabbcaf3efada38ace54a10d24
Richouf95 <wourichouf95@gmail.com>
2023-10-04 04:35:28 -0700

    Fix Copy a string 2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,6 +13,9 @@ char *_strdup(char *str)
 	int numberOfCharacter = 0;
 	char *strCopy;
 
+	if (str == NULL)
+		return (NULL);
+
 	while (str[numberOfCharacter] != '\0')
 		numberOfCharacter++;
 
@@ -20,7 +23,7 @@ char *_strdup(char *str)
 
 	strCopy = malloc(sizeof(char) * numberOfCharacter);
 
-	if (str == NULL || strCopy == NULL)
+	if (strCopy == NULL)
 		return (NULL);
 
 	for (i = 0; str[i]; i++)
