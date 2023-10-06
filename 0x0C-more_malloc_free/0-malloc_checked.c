d1d87347cd126e55fce465abbb33971922584354
Richouf95 <wourichouf95@gmail.com>
2023-10-05 23:53:45 -0700

    Memory allocation

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.>
+
+/**
+ * *malloc_checked - allocate memory with malloc
+ * @b: size of memory
+ * Return: 98 if fails, else the memory pointer
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *memoryPointer;
+
+	memoryPointer = malloc(b);
+
+	if (memoryPointer == NULL)
+		exit(98);
+
+	return (memoryPointer);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+More task on dynamic memory allocation on C
