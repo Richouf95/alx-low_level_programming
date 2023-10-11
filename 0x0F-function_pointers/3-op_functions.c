f1c75c5c54895755217eef40720f32c4134aa4ba
Richouf95 <wourichouf95@gmail.com>
2023-10-11 12:03:28 -0700

    Create calculatrice

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of two numbers
+ * @a: input
+ * @b: input
+ * Return: result
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub two number
+ * @a: input 
+ * @b: input
+ * Return: result
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplie two number
+ * @a: input
+ * @b: input
+ * Return: result
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divid two number
+ * @a: input
+ * @b: input
+ * Return: result
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - module of two number
+ * @a: input
+ * @b: input
+ * Return: result
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
