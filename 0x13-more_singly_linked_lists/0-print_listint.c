e91e2a346ea608c11dd5ad81001bb23a869b0c53
Richouf95 <wourichouf95@gmail.com>
2023-10-23 08:49:12 -0700

    Fix 0-print_listint.c 4

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t size = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		size++;
+		i++;
 		h = h->next;
 	}
 
-	return (size);
+	return (i);
 }
