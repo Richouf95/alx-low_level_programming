659a0f425e4973c6667ccd2dcbefcc2aad8a2d60
Richouf95 <wourichouf95@gmail.com>
2023-10-26 08:46:12 -0700

    Number of bit to change

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - number of bits to change
+ * @n: first
+ * @m: second
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int countbit = 0;
+	unsigned long int exclusive = n ^ m;
+
+	while (exclusive != 0)
+	{
+		countbit += exclusive & 1;
+		exclusive >>= 1;
+	}
+
+	return countbit;
+}
