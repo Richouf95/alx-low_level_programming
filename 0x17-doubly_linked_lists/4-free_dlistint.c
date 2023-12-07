6449eb690fa4a1b48a126b698b2f6f70f3c0b9b5
Richouf95 <wourichouf95@gmail.com>
2023-12-07 05:09:49 -0800

    Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free a list
+ * @head: list
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+	free(head);
+	head = NULL;
+}
