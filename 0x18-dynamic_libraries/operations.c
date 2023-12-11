f3d72ac575cf89daa5eee0f507b76fc48fcd4566
Richouf95 <wourichouf95@gmail.com>
2023-12-11 01:55:26 -0800

    Fusion python C

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,61 @@
+#include "operations.h"
+
+/**
+ * add - function add
+ * @a: param
+ * @b: param
+ * Return: sum
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function subtract
+ * @a: param
+ * @b: param
+ * Return: diff
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function multiply
+ * @a: param
+ * @b: param
+ * Return: product
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function divid
+ * @a: param
+ * @b: param
+ * Return: quotient
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function modulo
+ * @a: param
+ * @b: param
+ * Return: rest
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
