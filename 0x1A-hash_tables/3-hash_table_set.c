57f02afda6d1a096e58b86d45989e00c0ef54c8f
Richouf95 <wourichouf95@gmail.com>
2023-12-22 01:41:44 -0800

    Fix add item to a hash table 5

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,24 +10,22 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newNode, *temp;
-	unsigned long int index;
+	hash_node_t *newNode;
+	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	temp = ht->array[index];
-	while (temp != NULL)
+	for (i = index; ht->array[i]; i++)
 	{
-		if (strcmp(temp->key, key) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			free(temp->value);
-			temp->value = strdup(value);
-			if (temp->value == NULL)
+			free(ht->array[i]->value);
+			ht->array[i]->value = strdup(value);
+			if (ht->array[i]->value == NULL)
 				return (0);
 			return (1);
 		}
-		temp = temp->next;
 	}
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
