29ea3b92bf82e1f5bdfb0b0943a94c9279a87c06
Richouf95 <wourichouf95@gmail.com>
2023-12-22 00:03:58 -0800

    key index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - give the index of a key in hash table
+ * @key: the key
+ * @size the hash table array
+ * Return: the index
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned int value = hash_djb2(key);
+
+	return (value % size);
+}
