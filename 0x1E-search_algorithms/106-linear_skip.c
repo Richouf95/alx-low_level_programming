87bf7fda11da3241432dd835530f5b76a19c5399
Richouf95 <wourichouf95@gmail.com>
2024-04-11 15:05:41 -0700

    Skip search on list

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,50 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - linear skip
+ * @list: list
+ * @value: value
+ * Return: node
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *node, *target;
+
+	if (list == NULL)
+		return (NULL);
+
+	node = target = list;
+
+	while (target->n < value && target->next != NULL)
+	{
+		node = target;
+
+		if (target->express)
+		{
+			while (target->next != NULL)
+				target = target->next;
+		}
+		else
+		{
+			target = target->express;
+			printf("Value checked at index [%ld] = [%d]\n", target->index,
+					target->n);
+		}
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, target->index);
+
+	while (node->n < value && node->index < target->index)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+		node = node->next;
+	}
+
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	if (node->n == value)
+		return (node);
+
+	return (NULL);
+}
